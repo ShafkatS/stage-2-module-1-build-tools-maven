5ffcb514eb84ae57e6ed9bef86731f1a206f604c
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-29 15:58:36 +0500

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+      if(str.isEmpty()){
+            return false;
+        }
+        double d = Double.parseDouble(str);
+        return d>0;
     }
 }
