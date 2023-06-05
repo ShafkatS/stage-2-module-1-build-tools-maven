b3ff9e6d0c2ed35c635dc49083ed5f0e1cd04a2c
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 21:48:12 +0500

    Update Utils.java

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,27 +1,11 @@
 package com.epam.demo;
 
-import java.util.List;
 import com.epam.utils.StringUtils;
-import java.util.ArrayList;
+
+import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-         if(args == null){
-            return false;
-        }
-        List<String> temp = new ArrayList<>();
-        for(String s : args){
-            if(s.equals(" ")){
-                return false;
-            } else if (s.equals("")) {
-                return false;
-            } else if (s.equals("null")) {
-                return false;
-            }
-            if(StringUtils.isPositiveNumber(s)){
-                temp.add(s);
-            }
-        }
-        return temp.size() == args.size();
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
